a958d3eecdf20aea5e23d8212640a5863ca7e96d
bloomkail <arnaud.lambert@outlook.com>
2021-09-14 13:30:01 +0200

    hello world

---
+++ lab1/index.js
@@ -0,0 +1,16 @@
+// Import a module
+const http = require('http')
+
+// Declare an http server
+http.createServer(function (req, res) {
+
+  // Write a response header
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+  // Write a response content
+  res.end('Hello World\n');
+
+// Start the server
+}).listen(8080)
+
+// curl localhost:8080 or go to http://localhost:8080
