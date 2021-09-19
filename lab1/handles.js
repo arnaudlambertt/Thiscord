cfb5665e96fe55ea61d7bdbfdd07c7c834ccb2de
bloomkail <55895784+bloomkail@users.noreply.github.com>
2021-09-19 15:57:30 +0200

    added handles.js

---
+++ lab1/handles.js
@@ -0,0 +1,20 @@
+const url = require('url')
+const qs = require('querystring')
+
+module.exports = {
+  serverHandle : function (req, res) {
+    const route = url.parse(req.url)
+    const path = route.pathname
+    const params = qs.parse(route.query)
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+
+    if (path === '/hello' && 'name' in params) {
+      res.write('Hello ' + params['name'])
+    } else {
+      res.write('Hello anonymous')
+    }
+
+    res.end();
+  }
+}
--- lab1/index.js
+++ lab1/index.js
@@ -1,16 +1,4 @@
-// Import a module
 const http = require('http')
-
-// Declare an http server
-http.createServer(function (req, res) {
-
-  // Write a response header
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-
-  // Write a response content
-  res.end('Hello World\n');
-
-// Start the server
-}).listen(8080)
-
-// curl localhost:8080 or go to http://localhost:8080
+const handles = require('./handles')
+const server = http.createServer(handles.serverHandle);
+server.listen(8080)
