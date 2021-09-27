d58be625672070420564955bade4e572e91a1a07
bloomkail <55895784+bloomkail@users.noreply.github.com>
2021-09-27 12:07:32 +0200

    added error 404

--- lab1/handles.js
+++ lab1/handles.js
@@ -1,5 +1,6 @@
 const url = require('url')
 const qs = require('querystring')
+fs = require('fs')
 
 module.exports = {
   serverHandle : function (req, res) {
@@ -7,12 +8,27 @@ module.exports = {
     const path = route.pathname
     const params = qs.parse(route.query)
 
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+    //not asynchronous
+    var slash = fs.readFileSync('./slash.html', 'utf8')
+    var errorPage = fs.readFileSync('./404.html', 'utf8')
 
     if (path === '/hello' && 'name' in params) {
-      res.write('Hello ' + params['name'])
-    } else {
-      res.write('Hello anonymous')
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      if (params['name'] === 'arno')
+      {
+        res.write('Hello myself')
+      }
+      else
+      {
+        res.write('Hello ' + params['name']);
+      }
+    }
+    else if (path === '/') {
+        res.write(slash)
+    }
+    else {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      res.write(errorPage)
     }
 
     res.end();
