f3524ee208372f7dc484be388219780d71cb2190
bloomkail <55895784+bloomkail@users.noreply.github.com>
2021-09-28 14:35:15 +0200

    Patch directories and gitignore

--- lab1/handles.js
+++ lab1/handles.js
@@ -1,22 +1,22 @@
-const url = require('url')
-const qs = require('querystring')
-fs = require('fs')
+const url = require('url');
+const qs = require('querystring');
+fs = require('fs');
 
 module.exports = {
   serverHandle : function (req, res) {
-    const route = url.parse(req.url)
-    const path = route.pathname
-    const params = qs.parse(route.query)
+    const route = url.parse(req.url);
+    const path = route.pathname;
+    const params = qs.parse(route.query);
 
-    //not asynchronous
-    var slash = fs.readFileSync('./slash.html', 'utf8')
-    var errorPage = fs.readFileSync('./404.html', 'utf8')
+    //not asynchronous loading of html files
+    var slash = fs.readFileSync('./html/slash.html', 'utf8');
+    var errorPage = fs.readFileSync('./html/404.html', 'utf8');
 
     if (path === '/hello' && 'name' in params) {
       res.writeHead(200, {'Content-Type': 'text/html'});
       if (params['name'] === 'arno')
       {
-        res.write('Hello myself')
+        res.write('Hello myself');
       }
       else
       {
@@ -24,11 +24,11 @@ module.exports = {
       }
     }
     else if (path === '/') {
-        res.write(slash)
+        res.write(slash);
     }
     else {
       res.writeHead(404, {'Content-Type': 'text/html'});
-      res.write(errorPage)
+      res.write(errorPage);
     }
 
     res.end();
