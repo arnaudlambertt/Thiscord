b803187232e585c875273d1f504d95b549d7389d
bloomkail <55895784+bloomkail@users.noreply.github.com>
2021-10-17 22:40:12 +0200

    feat: messages export

--- front-end/src/Footer.js
+++ front-end/src/Footer.js
@@ -3,8 +3,8 @@ import { jsx } from '@emotion/core'
 import {styles} from './styles.js'
 
 export const Footer = () => {
-    return (
-      <footer className="App-footer" style={styles.footer}>
+  return (
+    <footer className="App-footer" style={styles.footer}>
       <p1>Footer</p1>
     </footer>
   );
