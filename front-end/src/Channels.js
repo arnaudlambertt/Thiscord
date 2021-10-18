38086574a686dde5ecd9099f1289382284ab4898
bloomkail <55895784+bloomkail@users.noreply.github.com>
2021-10-18 14:17:25 +0200

    feat: channel list - part 1 complete

--- front-end/src/Channels.js
+++ front-end/src/Channels.js
@@ -1,3 +1,30 @@
-export const channels = {
-  name: 'Fake channel'
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import {styles} from './styles.js'
+
+export const channels = [
+  {
+    name: 'Channel 1'
+  },
+  {
+    name: 'Channel 2'
+  },
+  {
+    name: 'Channel 3'
+  }
+]
+
+export const Channels = () => {
+  return (
+    <div css={styles.channels}>
+    <h1>Channel list</h1>
+    <ul>
+      { channels.map( (channel, i) => (
+        <li key={i} css={styles.channel}>
+          <span>{channel.name}</span>
+        </li>
+      ))}
+    </ul>
+    </div>
+  );
 }
--- front-end/src/Main.js
+++ front-end/src/Main.js
@@ -1,15 +1,14 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
 import {styles} from './styles.js'
-import {channels} from './Channels.js'
+import {Channels, channels} from './Channels.js'
 import {Channel} from './Channel.js'
 
 export const Main = () => {
     return (
       <main className="App-main" css={styles.main}>
-        <div css={styles.channels}>
-        </div>
-        <Channel channel={channels}/>
+        <Channels />
+        <Channel channel={channels[0]}/>
       </main>
     );
 }
