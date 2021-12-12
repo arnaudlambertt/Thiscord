bdb9abb89701af31f410635dbb5ffddb6c9ccb0f
bloomkail <55895784+bloomkail@users.noreply.github.com>
2021-12-12 20:00:45 +0100

    test: channels members not empty

--- back-end/test/channels.js
+++ back-end/test/channels.js
@@ -20,10 +20,13 @@ describe('channels', () => {
     })
 
     it('list one element', async () => {
+      // Sign in
+      const {body: user} = await supertest(app)
+      .get('/signin')
       // Create a channel
       await supertest(app)
       .post('/channels')
-      .send({name: 'channel 1', members: []})
+      .send({name: 'channel 1', members: [user.id]})
       // Ensure we list the channels correctly
       const {body: channels} = await supertest(app)
       .get('/channels')
@@ -31,23 +34,26 @@ describe('channels', () => {
       channels.should.match([{
         id: /^\w+-\w+-\w+-\w+-\w+$/,
         name: 'channel 1',
-        members: []
+        members: [user.id]
       }])
     })
 
   })
 
   it('create one element', async () => {
+    // Sign in
+    const {body: user} = await supertest(app)
+    .get('/signin')
     // Create a channel
     const {body: channel} = await supertest(app)
     .post('/channels')
-    .send({name: 'channel 1', members: []})
+    .send({name: 'channel 1', members: [user.id]})
     .expect(201)
     // Check its return value
     channel.should.match({
       id: /^\w+-\w+-\w+-\w+-\w+$/,
       name: 'channel 1',
-      members: []
+      members: [user.id]
     })
     // Check it was correctly inserted
     const {body: channels} = await supertest(app)
