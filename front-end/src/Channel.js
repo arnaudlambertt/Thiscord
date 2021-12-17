d030f295233e88f0b11b1634a93f3e4d678a7f2f
bloomkail <55895784+bloomkail@users.noreply.github.com>
2021-12-17 14:31:13 +0100

    feat: delete channel
    bad channel now redirects to home

--- front-end/src/Channel.js
+++ front-end/src/Channel.js
@@ -58,7 +58,7 @@ export default function Channel() {
           listRef.current.scroll()
         }
       }catch(err){
-        navigate('/oups')
+        navigate('/')
       }
     }
     fetch()
