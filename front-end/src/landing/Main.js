73186768308402373120e6e1d14cfc5f12c293aa
clementbouvard <bouvard.clement.92@gmail.com>
2021-12-19 19:05:21 +0100

    feat: landing page

---
+++ front-end/src/landing/Main.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+
+function Main() {
+
+  return (
+    <Grid
+      item
+      xs={12}
+      md={8}
+      sx={{
+        '& .markdown': {
+          py: 3,
+        },
+      }}
+    >
+      <Typography variant="h6" gutterBottom>
+        A project made by
+      </Typography>
+      <Divider />
+        <Typography>
+          Lambert Arnaud
+        </Typography>
+        <Typography>
+          Bouvard Clément
+        </Typography>
+    </Grid>
+  );
+}
+
+export default Main;
